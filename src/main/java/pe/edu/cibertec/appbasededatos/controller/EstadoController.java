0d7a9f371945569fb32ef2fea06078a983b505fa
CibertecT5LC <Cibertec_t5lc@outlook.es>
2023-05-07 09:59:57 -0500

    registro controller

--- src/main/java/pe/edu/cibertec/appbasededatos/controller/EstadoController.java
+++ src/main/java/pe/edu/cibertec/appbasededatos/controller/EstadoController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import pe.edu.cibertec.appbasededatos.model.bd.Estado;
@@ -30,6 +32,23 @@ public class EstadoController {
 		model.addAttribute("visualizar", false);
 		return "Estado/frmRegEstado";
 	}
+	
+	@PostMapping("/frmRegEstado")
+	public String registrarEstado(
+			@ModelAttribute("estadoForm")Estado estado,
+			Model model) {
+		String mensaje = "Estado registrado correctamente";
+		try {
+			estadoService.registrarEstado(estado);
+		}catch (Exception e) {
+			mensaje ="Estado no registrado";
+		}
+		model.addAttribute("estadoForm", new Estado());
+		model.addAttribute("visualizar", true);
+		model.addAttribute("respuesta", mensaje);
+		return "Estado/frmRegEstado";
+	}
+	
 	
 
 }
